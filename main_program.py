23766a5b0faa5b24932c3c0db8accf882237d351
madmax3000 <johnjose223@gmail.com>
2021-10-27 15:27:38 -0700

    added rungem5.sh

--- main_program.py
+++ main_program.py
@@ -4,7 +4,7 @@ localbp_predictor = "LocalBP"
 bi_predictor = 'BiModeBP'
 tbp_predictor = 'TournamentBP'
 bp_string = "NULL"
-shell_script_location = "./test1.sh"
+shell_script_location = "./runGem5.sh" #script location
 branchpredictor_replacement(bp_string, localbp_predictor)
 pro1 = subprocess.Popen([shell_script_location],shell= True)
 pro1.wait()
